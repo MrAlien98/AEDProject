2bc5d617780237c3d1fb519c0021260c0c8f42a2
Bryan Garcés <bragapo23@gmail.com>
2019-05-27 15:59:41 -0500

    the class BombingPoint has been fully commented

--- src/model/BombingPoint.java
+++ src/model/BombingPoint.java
@@ -1,22 +1,54 @@
 package model;
-
+/**
+ * This class represents a bombing point(represent a vertex)
+ * 
+ * 
+ * 
+ */
 public class BombingPoint implements Comparable<BombingPoint>{
 
+	/**
+	 * the String name that the class will contain
+	 * 
+	 * 
+	 * 
+	 */
 	private String name;
 	
+	
+	
+	/**
+	 * constructor method for the bombingPoint class 
+	 * 
+	 */
 	public BombingPoint(String name) {
 		this.name=name;
 	}
 
-
+	
+	/**
+	 * Method to get the name of the class
+	 * @return the String name of the class BombingPoint
+	 */
+	
 	public String getName() {
 		return name;
 	}
 
-
+	
+	/**
+	 * Method to modify the name of the class
+	 * @param the new String name that will replace the current name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	
+	/**
+	 * Method to return a String with the name of the class
+	 * @return the String name of the class BombingPoint
+	 */
 	
 	@Override
 	public String toString() {
